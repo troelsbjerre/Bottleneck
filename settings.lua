0c4b15012d9d7d3af27ec387b803d8073745f0f1
Marvin Lee Fimmel <kontakt@ekranos.me>
2018-01-23 21:56:38 +0100

    Added a setting to limit the amount of updates. This improves the performance a lot. For example: With a small map with science 1, 2 and military running Bottleneck needs 0.4ms to update. If we use the new setting to update every 4 ticks 0.12ms. That's 70% less. If we update just every 10 ticks we only need 0.06ms. That's 99,85% less with no noticable impact. I still see the flickering of some assemblers so I guess the update rate is still good enough.
    This setting should mostly help bigger saves, but even for my small save
    that is a significant gain.

--- settings.lua
+++ settings.lua
@@ -1,35 +1,44 @@
-data:extend{
-    {
-        type = "int-setting",
-        name = "bottleneck-signals-per-tick",
-        setting_type = "runtime-global",
-        default_value = 40,
-        maximum_value = 2000,
-        minimum_value = 1,
-        order = "bottleneck-ac[signals-per-tick]"
-    },
+data:extend {
+	{
+		type = "int-setting",
+		name = "bottleneck-update-every-x-ticks",
+		setting_type = "runtime-global",
+		default_value = 10,
+		maximum_value = 2000,
+		minimum_value = 1,
+		order = "bottleneck-ac[update-every-x-ticks]"
+	},
+	{
+		type = "int-setting",
+		name = "bottleneck-signals-per-tick",
+		setting_type = "runtime-global",
+		default_value = 40,
+		maximum_value = 2000,
+		minimum_value = 1,
+		order = "bottleneck-ac[signals-per-tick]"
+	},
 	{
 		type = "string-setting",
 		name = "bottleneck-show-running-as",
 		setting_type = "runtime-global",
 		default_value = "green",
-		allowed_values = { "off", "green", "red", "yellow",	"blue",	"redx","yellowmin","offsmall","greensmall","redsmall","yellowsmall","bluesmall","redxsmall","yellowminsmall"}, 
-        order = "bottleneck-ad[show-running-as]"
+		allowed_values = { "off", "green", "red", "yellow", "blue", "redx", "yellowmin", "offsmall", "greensmall", "redsmall", "yellowsmall", "bluesmall", "redxsmall", "yellowminsmall" },
+		order = "bottleneck-ad[show-running-as]"
 	},
 	{
 		type = "string-setting",
 		name = "bottleneck-show-stopped-as",
 		setting_type = "runtime-global",
 		default_value = "red",
-		allowed_values = { "off", "green", "red", "yellow",	"blue",	"redx","yellowmin","offsmall","greensmall","redsmall","yellowsmall","bluesmall","redxsmall","yellowminsmall"}, 
-        order = "bottleneck-ae[show-stopped-as]"
+		allowed_values = { "off", "green", "red", "yellow", "blue", "redx", "yellowmin", "offsmall", "greensmall", "redsmall", "yellowsmall", "bluesmall", "redxsmall", "yellowminsmall" },
+		order = "bottleneck-ae[show-stopped-as]"
 	},
 	{
 		type = "string-setting",
 		name = "bottleneck-show-full-as",
 		setting_type = "runtime-global",
 		default_value = "yellow",
-		allowed_values = { "off", "green", "red", "yellow",	"blue",	"redx","yellowmin","offsmall","greensmall","redsmall","yellowsmall","bluesmall","redxsmall","yellowminsmall"}, 
-        order = "bottleneck-af[show-full-as]"
+		allowed_values = { "off", "green", "red", "yellow", "blue", "redx", "yellowmin", "offsmall", "greensmall", "redsmall", "yellowsmall", "bluesmall", "redxsmall", "yellowminsmall" },
+		order = "bottleneck-af[show-full-as]"
 	},
 }
