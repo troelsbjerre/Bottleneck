787197f5123a7b65e6d4fe9f6c97c02358b1c386
Troels Bjerre Lund <trbj@itu.dk>
2019-03-22 22:16:35 +0100

    v0.10.1: listening for script create/destroy

--- control.lua
+++ control.lua
@@ -364,8 +364,8 @@ script.on_configuration_changed(on_configuration_changed)
 script.on_load(on_load)
 
 local e=defines.events
-local remove_events = {e.on_player_mined_entity, e.on_robot_pre_mined, e.on_entity_died}
-local add_events = {e.on_built_entity, e.on_robot_built_entity}
+local remove_events = {e.on_player_mined_entity, e.on_robot_pre_mined, e.on_entity_died, e.script_raised_destroy}
+local add_events = {e.on_built_entity, e.on_robot_built_entity, e.script_raised_revive, e.script_raised_built}
 
 script.on_event(remove_events, remove_signal)
 script.on_event(add_events, built)
